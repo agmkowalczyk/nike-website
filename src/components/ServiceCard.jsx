1413668154113115a3a1cce1da48f405fb02eab7
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-11-30 02:04:01 +0100

    Footer

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,6 @@
 const ServiceCard = ({ imgURL: imgUrl, label, subtext }) => {
   return (
-    <div className='flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16'>
+    <div className='flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16 hover:scale-105 transition-transform'>
       <div className='w-11 h-11 flex justify-center items-center bg-coral-red rounded-full'>
         <img src={imgUrl} alt={label} width={24} height={24} />
       </div>
